cc512ffe2582e32ba06e9b87e16b0b791e732127
TommasoPezzola <pezzola123@gmail.com>
2023-07-17 15:08:58 +0200

    Milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <script>
+import axios from 'axios';
+
 // import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: "App",
+  components: {
+
+  },
+  data() {
+    return {
+      ApiUrl: "http://localhost:8000/api/projects",
+    }
+  },
+  methods: {
+    getProjects() {
+      axios.get(this.ApiUrl).then(result =>{
+        console.log(result.data)
+      })
+    }
+  },
+  mounted() {
+    this.getProjects()
+  }
+}
 </script>
 
 <template>
