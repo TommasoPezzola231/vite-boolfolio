cf11957b12cdee7cda873b23cd9e5a977eaadf8d
TommasoPezzola <pezzola123@gmail.com>
2023-07-18 16:01:36 +0200

    feat: add content

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,31 @@
+<script>
+import AppHeader from "../components/AppHeader.vue"
+import AppFooter from '../components/AppFooter.vue'
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppFooter
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
+
+<template>
+
+    <h2 class="text-center">Home Page</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam iure porro corrupti perferendis molestias rerum, aspernatur quisquam iusto veritatis reiciendis molestiae, facere nemo odit ipsum, praesentium veniam pariatur deserunt. Repudiandae!</p>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/AppProjects.vue
@@ -0,0 +1,32 @@
+<script>
+import AppHeader from "../components/AppHeader.vue"
+import AppMain from "../components/AppMain.vue"
+import AppFooter from '../components/AppFooter.vue'
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
+
+<template>
+    
+    <AppMain />
+
+</template>
+
+<style scoped>
+
+</style>
