baf91fabc12811889fef42e97df007ebd933b434
TommasoPezzola <pezzola123@gmail.com>
2023-07-18 16:33:19 +0200

    feat: add content

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -22,6 +22,21 @@ export default {
 <template>
     <div class="card">
         <h4>{{ project.title }}</h4>
+        <h5>Tipo: <span class="badge text-bg-primary">{{ project.type.name ? project.type.name : 'Nessuno' }}</span></h5>
+
+        <h6>Tecnologie:
+
+          <template v-if="project.technologies.length > 0">
+            <span class="badge text-bg-info me-2" v-for="technology in project.technologies">
+              {{ technology.name }}
+            </span>
+          </template>
+          <span v-else>
+            <span class="badge text-bg-danger">Nessuna</span>
+          </span>
+
+        </h6>
+
         <p>{{ project.content }}</p>
     </div>
 </template>
