84b8e0fa42b3eea75ac14d42476d4c717a803a77
TommasoPezzola <pezzola123@gmail.com>
2023-07-18 16:00:11 +0200

    feat: add footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,127 @@
+<script>
+    export default {
+        name: "AppFooter",
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        mounted () {
+
+        }
+    }
+</script>
+
+<template>
+    <footer>
+      <div class="container-fluid bg-dark">
+        <div class="container">
+          <div class="row bg-dark">
+            <div class="col-3">
+              <div class="d-flex flex-column p-4">
+                <h3 class="text-light">Help</h3>
+                <ul class="list-unstyled mb-3">
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >Frequently asked questions</a
+                    >
+                  </li>
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >Proceed to Order</a
+                    >
+                  </li>
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >Payment Methods</a
+                    >
+                  </li>
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >Shipping Times</a
+                    >
+                  </li>
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >Where is my order located</a
+                    >
+                  </li>
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >How to make a return</a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-3">
+              <div class="d-flex flex-column p-4">
+                <h3 class="text-light">Gift vouchers</h3>
+                <ul class="list-unstyled mb-3">
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >Buy a gift certificate</a
+                    >
+                  </li>
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >Purchase Code</a
+                    >
+                  </li>
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >Corporate Gift Certificates</a
+                    >
+                  </li>
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >Gift Codes</a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-3">
+              <div class="d-flex flex-column p-4">
+                <h3 class="text-light">Online shopping is easy</h3>
+                <ul class="list-unstyled mb-3">
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >Free shipping and return*</a
+                    >
+                  </li>
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >Free return</a
+                    >
+                  </li>
+                  <li class="my-2">
+                    <a href="#" class="text-decoration-none text-light"
+                      >100 days for return</a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-3">
+              <div class="d-flex flex-column p-4">
+                <h3 class="text-light">Methods of payment</h3>
+                <img
+                  class="mt-3 img-fluid w-50"
+                  src="https://www.pngarts.com/files/4/PayPal-PNG-Download-Image.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
