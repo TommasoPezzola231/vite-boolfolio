dee9f1a34514a7c66cade7147b83c7f728a1bcf0
TommasoPezzola <pezzola123@gmail.com>
2023-07-18 16:00:23 +0200

    feat: add header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<script>
+    export default {
+        name: "AppHeader",
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        mounted () {
+
+        }
+    }
+</script>
+
+<template>
+
+    <nav class="navbar">
+        <div class="container-fluid d-flex justify-content-between p-3">
+
+            <span class="d-flex">
+                <a class="btn btn-secondary" href="/">Home</a>
+                <a class="btn btn-secondary ms-3" href="/projects">Projects</a>
+            </span>
+
+            <span class="d-flex">
+                <input type="search">
+                <button class="btn btn-primary" type="submit">Cerca</button>
+            </span>
+
+        </div>
+    </nav>
+
+</template>
+
+<style scoped>
+
+</style>
