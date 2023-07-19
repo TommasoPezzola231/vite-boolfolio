220ede9c760d09c66e9c647f43d9c45c9f72d7c2
TommasoPezzola <pezzola123@gmail.com>
2023-07-19 16:24:13 +0200

    Add show-project routes

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import AppHome from './pages/AppHome.vue'
 import AppProjects from './pages/AppProjects.vue'
 import NotFound from './pages/NotFound.vue'
+import ShowProject from './pages/ShowProject.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -16,6 +17,11 @@ const router = createRouter({
             path: '/projects',
             name: 'projects',
             component: AppProjects
+        },
+        {
+            path: '/projects/:id',
+            name: 'show-project',
+            component: ShowProject
         },
         {
             path: '/not-found',
